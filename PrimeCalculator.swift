a34dc4b83825e0b1364b16852501d5e346843b09
Jean-Nicolas <70505484+Fahrenberg@users.noreply.github.com>
2024-05-07 19:19:09 +0000

    new prime calc

---
+++ PrimeCalculator.swift
@@ -0,0 +1,33 @@
+//
+//  ------------------------------------------------------------
+//  ---------------     PrimeCalculatorTests         --------------
+//  ------------------------------------------------------------
+
+
+import Foundation
+
+struct PrimeCalculator {
+    static func calculate(upTo max: Int) -> [Int] {
+        guard max > 1 else {
+            return []
+        }
+
+        var sieve = [Bool](repeating: true, count: max)
+        sieve[0] = false
+        sieve[1] = false
+
+        for number in 2 ..< max {
+            if sieve[number] == true {
+                for multiple in stride(from: number * number, to: sieve.count, by: number) {
+                    sieve[multiple] = false
+                }
+            }
+        }
+
+        let primes = sieve.enumerated().compactMap { $1 == true ? $0 : nil }
+        return primes
+    }
+}
+
+let p = PrimeCalculator.calculate(upTo: 100)
+print(p)
